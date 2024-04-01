f03b69213af94af10707a4caee5334e165342e98
root <root@digitalkreatifsolusindo.cc>
2024-04-02 06:43:10 +0700

    draet dksindo web

--- app.vue
+++ app.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-    <div v-show="$pwa.needRefresh" class="flex justify-center py-12">
-      <span>
-        New content available, click on reload button to update.
-      </span>
-
-      <button class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" @click="$pwa.updateServiceWorker()">
-        Reload
-      </button>
-    </div>
     <NuxtPwaManifest />
     <NuxtPage />
   </div>
