a3bc2ac77101f37238d7abe32d4dcbc876036422
root <root@digitalkreatifsolusindo.cc>
2024-04-02 06:41:48 +0700

    draet dksindo web

--- app.vue
+++ app.vue
@@ -17,10 +17,26 @@
 
 <script setup>
 // If you want to use it in setup, import from the nuxtApp.
-const { $pwa } = useNuxtApp()
+  const { $pwa } = useNuxtApp()
 
-onMounted(() => {
-  if ($pwa.offlineReady)
-    confirm('App ready to work offline')
-})
+  useHead({
+    title: 'Healthy Diet',
+    meta: [
+      { name: 'description', content: 'Temukan informasi healthy diet dan konsultasikan masalah diet anda dengan Dokter profesional.' }
+      ],
+  });
+
+  useSeoMeta({
+    title: 'Healthy Diet',
+    ogTitle: 'Healthy Diet',
+    description: 'Dalam pengawasan dan bimbingan langsung dr. Ade Erni, M.Gizi, SpGK, AIFO-K yang berpengalaman ebagai spesialis gizi, saat ini Berpraktek langsung di Poli / Klinik Gizi RSUD Oto Iskandar Di Nata.',
+    ogDescription: 'Temukan informasi healthy diet dan konsultasikan masalah diet anda dengan Dokter profesional.',
+    ogImage: 'https://draet.dksindo.com/images/home-banner-mobile.svg',
+    twitterCard: 'summary_large_image',
+  })
+
+  onMounted(() => {
+    if ($pwa.offlineReady)
+      confirm('App ready to work offline')
+  })
 </script>
